1c13c69b9c72b9af945cb96fe0c8f4cbddb9ff7c
Sathishkumar Deena Kirupakaran <Sathishkumar_deenakirupakaran@comcast.com>
2026-04-21 15:48:43 -0400

    fix: add Namespace

--- src/gateway.cpp
+++ src/gateway.cpp
@@ -167,7 +167,7 @@ public:
             }
             else
             {
-                ErrorInfo errorInfo(static_cast<int32_t>(message["error"]["code"]), message["error"]["message"]);
+                Firebolt::ErrorInfo errorInfo(static_cast<int32_t>(message["error"]["code"]), message["error"]["message"]);
                 c->promise.set_value(
                     Result<nlohmann::json>{static_cast<Firebolt::Error>(message["error"]["code"]), errorInfo});
             }
