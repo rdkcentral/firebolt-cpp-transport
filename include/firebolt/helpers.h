469ac1324cd42d4234f55a4cdccf44d681fe5602
Sathishkumar Deena Kirupakaran <Sathishkumar_deenakirupakaran@comcast.com>
2026-04-22 05:15:20 -0400

    fix: Update helpers

--- include/firebolt/helpers.h
+++ include/firebolt/helpers.h
@@ -74,7 +74,7 @@ public:
         Result<nlohmann::json> result = getJson(methodName, parameters);
         if (!result)
         {
-            return Result<PropertyType>{result.error()};
+            return Result<PropertyType>{result.error(), result.errorInfo()};
         }
         try
         {
@@ -86,7 +86,7 @@ public:
         {
             FIREBOLT_LOG_ERROR("Getter", "Cannot parse data for a getter %s, payload: %s", methodName.c_str(),
                                result->dump().c_str());
-            return Result<PropertyType>{Firebolt::Error::InvalidParams};
+            return Result<PropertyType>{Firebolt::Error::InvalidParams, Firebolt::ErrorInfo(-32100, "Cannot parse data for a getter")};
         }
     }
 
